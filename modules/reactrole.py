eac3f9d246056a8c5dae6dc081d8082785029bd5
Matthew Davidson <mdavidson982@g.rwu.edu>
2023-12-06 12:28:44 -0500

    Current Changes

--- modules/reactrole.py
+++ modules/reactrole.py
@@ -2,6 +2,7 @@ import discord, requests, json, aiohttp
 from discord.ext import commands
 import Private as p
 import firebase_admin
+from google.cloud import firestore
 
 
 class reactClass(commands.Cog):
@@ -52,6 +53,8 @@ class reactClass(commands.Cog):
                 "roleID": str(roleData.id)
             })
             print(f"Role '{roleData}':'{roleData.id}' document with messageID '{messageID.id}' updated!")
+
+
         
         
     async def addRoleToUserDatabase(self, member, roleID, serverID): 
@@ -71,7 +74,7 @@ class reactClass(commands.Cog):
                     print("User Should Already Have The Role Assigned? Perchance the left and rejoined?")
                 else:
                     #print("Adding Role to User!")
-                    userDocRoles[serverID.id].extend(roleID)
+                    userDocRoles[str(serverID.id)].append(str(roleID))
                     userDoc.set({
                     "name":member.name,
                     "roles":userDocRoles,
@@ -101,7 +104,51 @@ class reactClass(commands.Cog):
                 
 
         
-
+    async def removeRoleToDatabase(self, member, roleID, serverID):
+        db = self.bot.client
+        userDoc = db.collection("User").document(str(member.id))
+        if userDoc.get().exists:
+            #print("User Doc Found!")
+            userDocData = userDoc.get().to_dict()
+            userDocRoles = userDocData.get("roles")
+            userDocServers = userDocData.get("servers")
+            #print(userDocRoles)
+            #print(userDocServers)
+            if str(serverID.id) in userDocServers and str(serverID.id) in userDocRoles:
+                #print("User In Server!")
+                UsersRoleInServer = userDocRoles[str(serverID.id)]
+                if roleID in UsersRoleInServer:
+                    #print("Adding Role to User!")
+                    userDocRoles[str(serverID.id)].remove(str(roleID))
+                    userDoc.set({
+                    "name":member.name,
+                    "roles":userDocRoles,
+                    "servers":userDocServers,
+                    "userID":str(member.id)
+                })
+                    print(f"Updated {member.name}:{member.id} document in User collection. Removed {roleID} to {serverID.id} key")
+                else:
+                    print("User shouldn't have this role to begin with?")
+                    
+            else:
+                print(f"User isn't in server. Adding server ID. BUT The user shouldn't have this role?")
+                userDocServers.append(str(serverID.id))
+                userDocRoles[str(serverID.id)] = []
+                userDoc.set({
+                "name":member.name,
+                "roles":userDocRoles,
+                "servers":userDocServers,
+                "userID":str(member.id)
+            })
+                print(f"Updated {member.name}:{member.id} document in User collection. Added {serverID.id} ignored {roleID}")
+        else:
+            userDoc.set({
+                "name":member.name,
+                "roles":{str(serverID.id) : []},
+                "servers":[str(serverID.id)],
+                "userID":str(member.id)
+            })
+            print(f"Added {member.name}:{member.id} document to User collection")
 
     
     @commands.command()
@@ -110,6 +157,73 @@ class reactClass(commands.Cog):
         message = await channel.send(roleData.mention)
         await self.addRoleToDatabase(ctx, message.guild.id, message, roleData)
         await message.add_reaction(p.EMOJI_CONFIRM)
+
+    @commands.command()
+    async def removerole(self, ctx, messageID):
+        db = self.bot.client
+        serverDoc = db.collection("Server").document(str(ctx.guild.id)).collection("roles").document(str(messageID))
+        if serverDoc.get().exists:
+            serverDoc.delete()
+
+
+    @commands.command()
+    @commands.has_permissions(administrator = True)
+    async def addAdminRoles(self, ctx):
+        db = self.bot.client
+        serverRef = db.collection("User")
+        membersList = ctx.message.mentions
+        rolesList = ctx.message.role_mentions
+        rolesIDList = []
+        for role in rolesList:
+            rolesIDList.append(str(role.id))
+        dataToAdd = {
+            str(ctx.guild.id):rolesIDList
+        }
+        for members in membersList:
+            userDoc = serverRef.document(str(members.id))
+            if userDoc.get().exists:
+                userDoc.update({"adminRoles":{str(ctx.guild.id):rolesIDList}})
+            else:
+                print("Adding User to table via addAdminRoles")
+                await self.removeRoleToDatabase(members, 0, ctx.guild)
+                userDoc.update({"adminRoles":{str(ctx.guild.id):rolesIDList}})
+            print(type(rolesList))
+            await members.add_roles(*rolesList)    
+               
+
+    @commands.command()
+    async def reinstate(self, ctx):
+        user = ctx.message.author
+        userID = user.id
+        db = self.bot.client
+        guild = ctx.guild
+        guildID = ctx.guild.id
+        userDoc = db.collection("User").document(str(userID))
+
+        if userDoc.get().exists:
+            userDocData = userDoc.get().to_dict()
+            userDocRoles = userDocData.get("roles")
+            userDocServers = userDocData.get("servers")
+            
+            
+            if str(ctx.guild.id) in userDocServers:
+                usersRoleInServer = userDocRoles[str(guildID)]
+                roles = guild.roles
+                for role in roles:
+                    if str(role.id) in usersRoleInServer:
+                        await ctx.author.add_roles(role)
+
+            else:
+                print("Reinstate user wasn't in the server. Ignoring command and adding user to database")
+                userDoc.set({
+                    "name":user.name,
+                    "roles":userDocRoles,
+                    "servers":userDocServers.append(guildID),
+                    "userID":str(user.id)
+                })
+        else:
+            await ctx.send("You new here?") #add the user to the firebase server. If the user isn't in the doc it uses the bottom condition
+            self.removeRoleToDatabase(user,0, guildID)
     
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
@@ -131,16 +245,38 @@ class reactClass(commands.Cog):
                     print("Not In Document")
             else:
                 print("Message ID not found in role subcollection")
+
         else:
             print("Reaction added to react role")
         
-        #await channel.send("Bing Bong!")
+        
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
         dataCache = self.parse_reaction_payload(payload)
-        channel = self.bot.get_channel(dataCache[1])
-        #await channel.send("Bong Bing!")
+        guild = self.bot.get_guild(dataCache[0])
+        member = await guild.fetch_member(payload.user_id)
+        if not member.bot:
+            db = self.bot.client
+            #print(dataCache[2])
+            serverDoc = db.collection("Server").document(str(dataCache[0])).collection("roles").document(str(dataCache[2]))
+            if serverDoc.get().exists: #if a document with that message id exist
+                serverDocData = serverDoc.get().to_dict()
+                server = self.bot.get_guild(dataCache[0])
+                roleID = serverDocData.get('roleID')
+                if roleID:
+                    role = server.get_role(int(roleID))
+                    await member.remove_roles(role)
+                    await self.removeRoleToDatabase(member, roleID, server)
+
+                else:
+                    print("Not In Document")
+            else:
+                print("Message ID not found in role subcollection")
+        else:
+            print("Bot Removed Emoji")
+
+        
 
     
 
